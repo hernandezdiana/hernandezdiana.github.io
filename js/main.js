5032c6c6e7cab2364856b32fdb2e457f4e47952a
Diana Hernandez <DianaHernandezJ@gmail.com>
2017-08-16 18:55:02 -0400

    first

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//When user scrolls down to about me section
+  //Navigation bar is no longer transparent and becomes sticky
+    //Navigation links move to the right of the navigation bar
+      //And my name appears on the left side of the navigation bar
