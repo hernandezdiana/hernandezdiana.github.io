2dea4256f983b10f0d483fbfd42fa84b669ec645
Diana Hernandez <DianaHernandezJ@gmail.com>
2017-08-20 22:27:54 -0400

    Much Better

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-//When user scrolls down to about me section
-  //Navigation bar is no longer transparent and becomes sticky
-    //Navigation links move to the right of the navigation bar
-      //And my name appears on the left side of the navigation bar
+//When user scrolls
+$(window).on('scroll', function () {
+  var distanceScrolled = $(window).scrollTop();
+  console.log(distanceScrolled);
+  // IF the user has scrolled more than 104 (distanceScrolled is greater than 104)
+  if (distanceScrolled > 117) {
+    // Navigation bar is no longer transparent and becomes sticky (add a class)
+    $('nav').addClass('navbar');
+    // Fade in  my name appears on the left side of the navigation bar
+    $('.myname').fadeIn(500);
+    // Navigation links move to the right of the navigation bar (add a class)
+  // else
+  } else {
+    // Navigation bar is no longer sticky and becomes transparent (remove a class)
+    $('nav').removeClass('navbar');
+    // Fade out my name on the left side of the navigation bar
+    $('.myname').fadeOut('fast');
+  }
+});
